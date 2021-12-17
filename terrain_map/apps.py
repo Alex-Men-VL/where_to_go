09fd59177bd981bc2be8cd643ec848082d5b81dc
Aleksandr <menshikov.a.v@icloud.com>
2021-12-17 21:59:55 +0300

    Add blank start page

---
+++ terrain_map/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TerrainMapConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'terrain_map'
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+
 from environs import Env
 
 env = Env()
