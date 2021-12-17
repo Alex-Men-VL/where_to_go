bd3503d13b7d73b994d2304f029d3942fa786c2e
Aleksandr <menshikov.a.v@icloud.com>
2021-12-17 23:19:46 +0300

    Add Person model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField('Заголовок', max_length=100)
+    description_short = models.TextField('Короткое описание')
+    description_long = models.TextField('Длинное описание')
+    coordinate_lng = models.FloatField('Долгота')
+    coordinate_lat = models.FloatField('Широта')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'место'
+        verbose_name_plural = 'места'
+
